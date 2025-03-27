42c727f5e103077d269bbaf7b9d680d60f72ee68
Samson Odejide <odejideabiodun7@gmail.com>
2025-03-27 08:26:25 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function ToDOList() {
+  const [tasks, setTasks] = useState([
+    "Morning prayer",
+    "Workout",
+    "Break-fast",
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() !== "") setTasks((t) => [...t, newTask]);
+    setNewTask("");
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="add-botton" onClick={addTask}>
+          Add
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+export default ToDOList;
